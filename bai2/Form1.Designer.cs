de4e5acd88d9ae08f132e3b72f233627fd82abcc
PC819 <PC819@PC819>
2024-08-20 10:51:34 +0700

    Add project files.

---
+++ bai2/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace bai2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rd_do = new System.Windows.Forms.RadioButton();
+            this.rd_xanh = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.rd_xanh);
+            this.groupBox1.Controls.Add(this.rd_do);
+            this.groupBox1.Location = new System.Drawing.Point(314, 71);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(150, 127);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chon Mau";
+            // 
+            // rd_do
+            // 
+            this.rd_do.AutoSize = true;
+            this.rd_do.Location = new System.Drawing.Point(6, 30);
+            this.rd_do.Name = "rd_do";
+            this.rd_do.Size = new System.Drawing.Size(39, 17);
+            this.rd_do.TabIndex = 0;
+            this.rd_do.Text = "Do";
+            this.rd_do.UseVisualStyleBackColor = true;
+            this.rd_do.CheckedChanged += new System.EventHandler(this.rd_nam_CheckedChanged);
+            // 
+            // rd_xanh
+            // 
+            this.rd_xanh.AutoSize = true;
+            this.rd_xanh.Checked = true;
+            this.rd_xanh.Location = new System.Drawing.Point(6, 53);
+            this.rd_xanh.Name = "rd_xanh";
+            this.rd_xanh.Size = new System.Drawing.Size(50, 17);
+            this.rd_xanh.TabIndex = 1;
+            this.rd_xanh.TabStop = true;
+            this.rd_xanh.Text = "Xanh";
+            this.rd_xanh.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "To Mau";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(512, 82);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 125);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rd_do;
+        private System.Windows.Forms.RadioButton rd_xanh;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ bai2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rd_nam_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (rd_do.Checked)
+            {
+                textBox1.BackColor = Color.Red;
+
+            }
+            else
+                textBox1.BackColor = Color.Blue;
+        }
+    }
+}
